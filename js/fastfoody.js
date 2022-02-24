613fec5c503d16c81504c727b1d35736e7ca945c
hasanrafi69 <hasanrafi69@gmail.com>
2022-02-24 22:21:57 +0600

    add category

--- js/fastfoody.js
+++ js/fastfoody.js
@@ -4,6 +4,11 @@
  * Email: hasanrafi69@gmail.com
  * Mobile: +880-161-782323
  */
+
+// All Variable
+// const categoryProductList = document.getElementById('productlist');
+const row = document.getElementById('row');
+
 // =========Fetch Food Name==========
 const category = () => {
     fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
@@ -11,16 +16,15 @@ const category = () => {
         .then(data => displayCategory(data))
 }
 const displayCategory = (categorlist) => {
-    const categories = document.getElementById('categories');
     // all category
     const allcategory = categorlist.categories;
-    // console.log(allcategory);
     for (const category of allcategory) {
         // console.log(category.strCategory);
         const div = document.createElement('div');
         div.classList.add('col-lg-4')
         div.classList.add('col-6')
         div.innerHTML = `
+        <a onclick="categoryProduct()">
         <div class="card mb-3" style="max-width: 540px;">
             <div class="row g-0">
                 <div class="col-md-4">
@@ -34,8 +38,9 @@ const displayCategory = (categorlist) => {
                 </div>
             </div>
         </div>
+        </a>
         `
-        categories.appendChild(div)
+        row.appendChild(div)
     }
 
 }
@@ -44,5 +49,27 @@ const displayCategory = (categorlist) => {
 const categoryProduct = () => {
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => categoryProductList(data.meals))
+}
+const categoryProductList = (productlists) => {
+    row.innerHTML = ''
+    for (const productlist of productlists) {
+        console.log(productlist)
+        const div = document.createElement('div');
+        div.classList.add('col-lg-4')
+        div.innerHTML = `
+        <div class="card py-1">
+            <img src="${productlist.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${productlist.strMeal}</h5>
+            </div>
+            <div class="card-footer w-100">
+                <button class="btn btn-primary">View</button>
+            </div>
+        </div>
+    `
+        row.appendChild(div);
+    }
+
+
 }
